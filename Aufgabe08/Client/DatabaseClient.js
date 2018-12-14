71a0e48162979b7d88edcb8bc17acd4cada44d6d
monettamarchiano <monettamarchiano@monettas-mbp.eduroam.hs-furtwangen.de>
2018-12-14 13:46:32 +0100

    Aufgabe 8.2

--- Aufgabe08/Client/DatabaseClient.js
+++ Aufgabe08/Client/DatabaseClient.js
@@ -2,7 +2,7 @@ var DatabaseClient;
 (function (DatabaseClient) {
     window.addEventListener("load", init);
     // let serverAddress: string = "http://localhost:8100";
-    let serverAddress = "https://eia-2-marchian.herokuapp.com";
+    let serverAddress = "https://studivz.herokuapp.com";
     function init(_event) {
         console.log("Init");
         let insertButton = document.getElementById("insert");
